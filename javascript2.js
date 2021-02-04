fc023001de75193e03fce4ef605cd2e1f9a6e53a
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-02-03 19:17:47 -0800

    index and javascript

--- javascript2.js
+++ javascript2.js
@@ -1,1 +1,29 @@
-let newBranch = "working"
+
+// Edamame API setup
+var APIKey = "458ba6347df752837e263807d257869b"
+appId = "14bab965"
+var edamameURL = "https://api.edamam.com/search?q="
+
+var searchEl = document.querySelector("#searchBtn");
+var diet = document.querySelector("#dietSearch");
+
+function getRecipe(event) {
+    event.preventDefault();
+
+    var edamame = edamameURL + diet + appId + APIKey;
+
+    console.log(edamame);
+
+    fetch(edamame)
+
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+
+            var recipes = document.querySelector("#food");
+            console.log(recipes);
+        })
+};
+searchEl.addEventListener("click", getRecipe);
