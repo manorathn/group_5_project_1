2856fa4af4f435071254793e4547a8f12151f55c
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-02-03 23:08:09 -0800

    API Working

--- javascript2.js
+++ javascript2.js
@@ -1,29 +1,40 @@
 
 // Edamame API setup
-var APIKey = "458ba6347df752837e263807d257869b"
-appId = "14bab965"
-var edamameURL = "https://api.edamam.com/search?q="
+const APIKey = "458ba6347df752837e263807d257869b"
+const appId = "14bab965"
+const edamameURL = "https://api.edamam.com/search?q="
 
+
+var searchForm = document.querySelector('form');
 var searchEl = document.querySelector("#searchBtn");
-var diet = document.querySelector("#dietSearch");
+let searchQuery = '';
 
-function getRecipe(event) {
+searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    searchQuery = event.target.querySelector("input").value;
+    console.log(searchQuery);
+    fetchAPI()
+});
 
-    var edamame = edamameURL + diet + appId + APIKey;
+async function fetchAPI() {
+    const edamame = edamameURL + searchQuery + '&app_id=' + appId + '&app_key=' + APIKey + '&to=10';
+    // console.log(edamame);
+    const response = await fetch(edamame);
+    console.log(response);
+    const data = await response.json();
+    // generateHTML(data.hits);
+    console.log(data);
+}
 
-    console.log(edamame);
+// function generateHTML(results) {
+//     const generateHTML = '';
+//     results.map(result => {
+//         generateHTML +=
+//             "
 
-    fetch(edamame)
+//         "
 
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
+//     })
+// }
 
-            var recipes = document.querySelector("#food");
-            console.log(recipes);
-        })
-};
-searchEl.addEventListener("click", getRecipe);
+searchEl.addEventListener("click", fetchAPI);
