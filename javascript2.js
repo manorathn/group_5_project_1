6c7d49aafb0c668ac4e8cc025ce04b014155d2b5
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-02-06 11:25:16 -0600

    API 2

--- javascript2.js
+++ javascript2.js
@@ -45,7 +45,19 @@ async function fetchAPI() {
     const data = await response.json();
     // generateHTML(data.hits);
     console.log(data);
-}
+};
+
+var secondQuery = "https://api.spoonacular.com/recipes/random?apiKey=81188f168e174959b542fc55bff0ccd6"
+
+async function randomAPI(){
+    const spoonacular = secondQuery
+    const response = await fetch(spoonacular);
+    console.log("@nd Query" , response);
+
+    const data = await response.json();
+
+    console.log(data);
+};
 
 // function generateHTML(results) {
 //     const generateHTML = '';
