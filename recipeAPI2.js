067a21827b732ee6deb5f604415e453c062ac5d4
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-02-06 20:59:46 -0600

    Modal for Homepage

--- recipeAPI2.js
+++ recipeAPI2.js
@@ -1,3 +1,6 @@
+
+
+// API number 2
 var secondQuery =
   "https://api.spoonacular.com/recipes/random?apiKey=81188f168e174959b542fc55bff0ccd6";
 
@@ -9,7 +12,30 @@ async function randomAPI() {
   const data = await response.json();
 
   console.log(data);
+};
+
+// Modal Javascript
+var modal = document.getElementById("simple-modal");
+var modalBtn = document.getElementById("modal-button");
+var closeBtn = document.getElementsByClassName("close-button")[0];
+
+
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+
+function openModal() {
+  modal.style.display = "block";
 }
 
+function closeModal() {
+  modal.style.display = "none";
+}
+function outsideClick(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 searchEl.addEventListener("click", randomAPI);
