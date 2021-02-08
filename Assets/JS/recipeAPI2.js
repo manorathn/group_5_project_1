f8ca5668652401a0fa365de6bc0065369700825b
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-02-07 18:51:33 -0800

    secondAPI and modal updates

--- Assets/JS/recipeAPI2.js
+++ Assets/JS/recipeAPI2.js
@@ -12,7 +12,34 @@ async function randomAPI() {
   const data = await response.json();
 
   console.log(data);
+
+  // title
+  var title = document.querySelector(".title0");
+  // console.log(img);
+  title.textContent = data.recipes[0].title;
+  console.log(data.recipes[0].title);
+
+  // image
+  var img0 = document.querySelector(".img0");
+  img0.src = data.recipes[0].image;
+  console.log(data.recipes[0].image);
+
+  // direction
+  var direction = document.querySelector(".direction")
+  direction.textContent = data.recipes[0].instructions;
+  console.log(data.recipes[0].instructionss);
+
+
+
+  // prep
+  var prep = document.querySelector(".prep0");
+  // console.log(img);
+  prep.textContent = "Prep: " + data.recipes[0].readyInMinutes + " mins";
+  console.log(data.recipes[0].readyInMinutes);
+
 };
+randomAPI();
+
 
 // Modal Javascript
 var modal = document.getElementById("simple-modal");
